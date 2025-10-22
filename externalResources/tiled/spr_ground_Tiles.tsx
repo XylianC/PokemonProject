b7a7a5a04cae0a29cf795a1e78dcc508d83c4b95
xylian <xylian.cocquyt@student.hogent.be>
2025-10-22 11:12:18 +0200

    - see previous commit

---
+++ externalResources/tiled/spr_ground_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="spr_ground_Tiles" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="pokemon-game/res/tileSets/spr_ground_Tiles.png" width="64" height="64"/>
+</tileset>
